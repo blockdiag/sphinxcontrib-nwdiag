191e7a32df5cff8b4eb7f192b9684f77e662b669
tk0miya <i.tkomiya@gmail.com>
2014-10-17 15:07:42 +0900

    nwdiag: Update README

--- nwdiag/setup.py
+++ nwdiag/setup.py
@@ -2,46 +2,6 @@
 
 from setuptools import setup, find_packages
 
-long_desc = '''
-This package contains the nwdiag Sphinx extension and rackdiag Sphinx extension.
-
-.. _Sphinx: http://sphinx.pocoo.org/
-.. _nwdiag: http://blockdiag.com/en/nwdiag/
-
-This extension enable you to insert network diagrams into your Sphinx document.
-Following code is sample::
-
-   .. nwdiag::
-
-      diagram {
-        network {
-          web01; web02;
-        }
-        network {
-          web01; web02; db01;
-        }
-      }
-
-And insert rack structure diagrams::
-
-   .. rackdiag::
-
-      diagram {
-        rackheight = 12;
-
-        1: UPS [height = 3];
-        4: DB Server (Master)
-        5: DB Server (Mirror)
-        7: Web Server (1)
-        8: Web Server (1)
-        9: Web Server (1)
-        10: LoadBalancer
-      }
-
-
-This module needs nwdiag_.
-'''
-
 requires = ['nwdiag>=1.0.3', 'Sphinx>=0.6', 'setuptools']
 
 setup(
@@ -52,8 +12,8 @@ setup(
     license='BSD',
     author='Takeshi Komiya',
     author_email='i.tkomiya@gmail.com',
-    description='Sphinx "nwdiag" and "rackdiag" extension',
-    long_description=long_desc,
+    description='Sphinx "nwdiag" extension',
+    long_description=open('README.rst').read(),
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
