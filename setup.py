afa10d49682348125e2071ef77f286eb7dd3e646
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-04-18 18:13:35 +0900

    Move to github

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ requires = ['blockdiag>=1.5.0', 'nwdiag>=1.0.3', 'Sphinx>=0.6']
 setup(
     name='sphinxcontrib-nwdiag',
     version='0.9.1',
-    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
-    download_url='http://pypi.python.org/pypi/sphinxcontrib-nwdiag',
+    url='http://github.com/tk0miya/sphinxcontrib-nwdiag',
     license='BSD',
     author='Takeshi KOMIYA',
     author_email='i.tkomiya@gmail.com',
