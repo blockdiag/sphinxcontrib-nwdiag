99aeafaa141891caf301ce2953b420f762ea233f
Takeshi KOMIYA <i.tkomiya@gmail.com>
2021-12-07 02:30:18 +0900

    Drop py36 support

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Documentation',
@@ -36,6 +34,7 @@ setup(
     platforms='any',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
+    python_requires=">=3.7",
     install_requires=requires,
     namespace_packages=['sphinxcontrib'],
 )
